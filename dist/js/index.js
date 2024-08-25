cdca416bac032c8177b68132f114e829b00d91e3
Abimael Cristovao <abimael.servicos12dt@gmail.com>
2024-08-25 15:25:57 -0300

    primeiro commit

---
+++ dist/js/index.js
@@ -0,0 +1,35 @@
+"use strict";
+// Definindo o texto e a imagem para cada escolha
+const infoDados = {
+    mundoReal: {
+        texto: "Você escolheu o Mundo Real. Neste mundo, a humanidade vive fora da Matrix, lutando pela sobrevivência contra as máquinas que controlam a Matrix.",
+        imagem: "/src/imagens/mundoReal.png" // Atualize o caminho para a imagem do Mundo Real
+    },
+    matrix: {
+        texto: "Você escolheu a Matrix. Neste mundo, você vive em uma realidade simulada criada pelas máquinas, onde a maioria da humanidade vive sem saber a verdade.",
+        imagem: "/src/imagens/matrix.png"
+    }
+};
+const mundoRealButton = document.getElementById('mundoRealButton');
+const matrixButton = document.getElementById('matrixButton');
+const infoSection = document.getElementById('infoSection');
+const infoTextoElement = document.getElementById('infoTexto');
+const infoImagem = document.getElementById('infoImagem');
+mundoRealButton.addEventListener('click', () => mostrarInfo('mundoReal'));
+matrixButton.addEventListener('click', () => mostrarInfo('matrix'));
+function mostrarInfo(escolha) {
+    const { texto, imagem } = infoDados[escolha];
+    infoTextoElement.textContent = texto;
+    infoImagem.src = imagem;
+    infoImagem.classList.remove('hidden');
+    infoSection.classList.remove('hidden');
+    // Remove todas as classes de cor anteriores
+    infoTextoElement.classList.remove('textoAzul', 'textoVermelho');
+    // Adiciona a classe de cor apropriada
+    if (escolha === 'mundoReal') {
+        infoTextoElement.classList.add('textoAzul');
+    }
+    else if (escolha === 'matrix') {
+        infoTextoElement.classList.add('textoVermelho');
+    }
+}
